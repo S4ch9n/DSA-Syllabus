89a9ab9e9b5d0f931477096b9992f70b4a74463c
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2024-07-10 04:59:27 +0530

    a

---
+++ Arrays/2DArrays/iteratejoutside.ts
@@ -0,0 +1,13 @@
+function iterate(arr : number[][]) : void{
+  for(let j = 0 ; j < arr[0].length ; j++){
+    for(let i = arr.length - 1 ; i >= 0  ; i--){
+      console.log(arr[i][j])
+    }
+  }
+}
+let arr=[
+  [2,3,6,2],
+  [1,4,3,7],
+  [3,2,9,5]
+]
+iterate(arr)
